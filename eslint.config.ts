80387dd5ff8b1dc36e379e2fbfb4a6fd978a8cbd
Eddie Villanueva <evillan0315@gmail.com>
2025-08-17 23:04:17 +0800

    Added a loading spinner icon to file explorer nodes that are folders and are currently fetching their children.

--- eslint.config.ts
+++ eslint.config.ts
@@ -28,7 +28,7 @@ export default tseslint.config([
     },
     plugins: {
       'react-hooks': reactHooks,
-      //'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh,
       'unused-imports': unusedImports,
     },
     rules: {
